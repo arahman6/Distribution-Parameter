eff0cdcc4233c8f3b55021126c076c41c559ec78
arahman6 <arahman.isrt@gmail.com>
2016-02-22 07:02:00 +0600

    Added files via upload

--- server.R
+++ server.R
@@ -57,12 +57,12 @@ function(input, output, session) {
         sliderInput("par1", label =  HTML(paste(par1_exp[distrib_name==input$distName])),
                     par1_min[distrib_name==input$distName],
                     par1_max[distrib_name==input$distName],
-                    par1_default[distrib_name==input$distName],step = 0.1)
+                    par1_default[distrib_name==input$distName],step = step[distrib_name==input$distName])
     )
     
     output$sliderPar2 <- renderUI({
         if(number_par[distrib_name==input$distName]>=2){
-                sliderInput("par2", "Varience of Normal Distribution:",
+                sliderInput("par2", label =  HTML(paste(par2_exp[distrib_name==input$distName])),
                     par2_min[distrib_name==input$distName],
                     par2_max[distrib_name==input$distName],
                     par2_default[distrib_name==input$distName],step = 0.1)
@@ -72,7 +72,7 @@ function(input, output, session) {
     
     output$sliderPar3 <- renderUI({
         if(number_par[distrib_name==input$distName]>=3){
-            sliderInput("par3", "Varience of Normal Distribution:",
+            sliderInput("par3", label =  HTML(paste(par2_exp[distrib_name==input$distName])),
                         par3_min[distrib_name==input$distName],
                         par3_max[distrib_name==input$distName],
                         par3_default[distrib_name==input$distName],step = 0.1)
@@ -92,7 +92,22 @@ function(input, output, session) {
                 fill = TRUE
         )
     )
+    gen_dat <- reactive(
+        rdf[distrib_name==input$distName]
+    )
+    output$infoMean_gen <- renderInfoBox(
+        infoBox(
+            "Mean:", round(mean(gen_dat()$values),4), icon = icon("list"),
+            color = "purple", fill = TRUE
+        )
+    )
     
+    output$infoVarience_gen <- renderInfoBox(
+        infoBox(
+            "Varience:", round(var(gen_dat()$values),2), icon = icon("list"),
+            color = "purple", fill = TRUE
+        )
+    )
     output$infoVarience <- renderInfoBox(
         infoBox("Varience:",
                 switch(input$distName,
